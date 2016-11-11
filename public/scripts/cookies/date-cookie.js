f31c368c50056fae4917a6b8d402a286c7e65e3f
pstrawberriedev <pstrawberriedev@gmail.com>
2016-11-11 14:27:39 -0700

    initial up

---
+++ public/scripts/cookies/date-cookie.js
@@ -0,0 +1,43 @@
+/*
+  Date Cookie (yum!)
+*/
+
+
+console.log('--> date-cookies.js');
+
+// Set up dates via moment.js
+//
+var currentDate = Math.round(new Date().getTime());
+var formattedDate = moment(currentDate).format('MMM Do YYYY hh:mma');
+
+// Initial cookie check - make sure we've got a cookie, if not create a new one
+//
+var dateCookie = Cookies.getJSON('holly-date');
+
+// Cookie Exists
+if(dateCookie != undefined) {
+  
+  console.log('Date Cookie exists: ' + dateCookie);
+  console.log('last visited: ' + dateCookie.visited);
+  var lastVisited = moment(dateCookie.visited, 'MMM Do YYYY hh:mma').fromNow();
+  
+  // Greet User
+  $('main header[role=banner]').prepend('Your last visit: ' + lastVisited + '<br />');
+  
+  // Reset Last visited date
+  setTimeout(function() {
+    Cookies.set('holly-date', { visited: formattedDate }, { expires: 365 });
+  },100);
+  
+} else {
+  
+  console.log('Creating new Date Cookie');
+  Cookies.set('holly-date', { visited: formattedDate }, { expires: 365 });
+  
+  dateCookie = Cookies.getJSON('holly-date');
+  console.log('New Date Cookie Created: ' + dateCookie);
+  
+  // Greet User
+  $('main header[role=banner]').prepend('You haven\'t visited yet. Welcome!<br />');
+  
+}
---
+++ public/scripts/cookies/search-cookie.js
@@ -0,0 +1,93 @@
+/*
+  Search Cookie (yum!)
+*/
+
+
+console.log('--> search-cookies.js');
+
+// Define search UI variables
+//
+var $searchBox = $('#search-box');
+var $recentContainer = $('#recent-searches');
+var $recentBox = $('#recent-searches ul');
+
+// Check if the user has a search cookie
+//
+var searchCookie = Cookies.getJSON('holly-search');
+
+if(searchCookie != undefined) {
+
+  var recentArray = searchCookie.searches;
+  if(recentArray.length > 0) { arrayToList(); }
+  console.log('Search Cookie exists: ' + recentArray);
+  
+} else {
+  
+  console.log('Creating new search cookie.');
+  Cookies.set('holly-search', { searches: [] }, { expires: 365 });
+  
+  setTimeout(function() {
+    searchCookie = Cookies.getJSON('holly-search');
+    var recentArray = searchCookie.searches;
+    console.log('Search Cookie created: ' + recentArray);
+  },50);
+    
+}
+
+// UI: 'Recently Searched' hide/show Functionality
+//
+$searchBox.on('focus', function() { 
+  if(recentArray.length > 0) {$recentBox.find('li.filler').hide();}
+  $recentBox.fadeIn();
+  $recentContainer.fadeIn();
+});
+$searchBox.on('blur', function() { 
+  $recentBox.fadeOut();
+  $recentContainer.fadeOut();
+});
+
+// Format Array into HTML
+function arrayToList() {
+  $recentBox.html('');
+  $.each(recentArray, function(i) {
+      var li = $('<li/>')
+          .text(recentArray[i])
+          .appendTo($recentBox);
+  });
+}
+
+// Capture Searches on Enter key
+$searchBox.keyup(function (e) {
+  if (e.which === 13) {
+
+      var searchText = $searchBox.val();
+
+      // Make sure our array doesn't have more than 5 items
+      // - if it does, delete the oldest one and push in the new item
+      // - if it doesn't, just push in the new item
+      if(recentArray.length >= 5) {
+
+        var firstItem = recentArray[0];
+        var index = recentArray.indexOf(firstItem);
+        recentArray.splice(index, 1);
+
+        setTimeout(function() {
+          recentArray.push(searchText);
+
+          setTimeout(function() {
+            Cookies.set('holly-search', { searches: recentArray }, { expires: 365 });
+            arrayToList();
+          },50);
+
+        },100);
+
+      } else {
+        recentArray.push(searchText);
+        setTimeout(function() {
+          Cookies.set('holly-search', { searches: recentArray }, { expires: 365 });
+          arrayToList();
+        },100);
+      }
+
+  }
+});
