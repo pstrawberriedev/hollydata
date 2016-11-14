c10b26902f2f06e20425724e5495cf6efc33c9e0
JD <pstrawberrie@gmail.com>
2016-11-13 22:59:12 -0700

    made the first call, yay

--- public/scripts/wow/test-wow.js
+++ public/scripts/wow/test-wow.js
@@ -5,38 +5,118 @@
 
 
 console.log('--> test-wow.js');
-console.log('[test-wow.js is mean for local testing - API Key exposed]');
+console.warn('[test-wow.js - Careful, API Key is exposed!]');
 
 // Define WoW UI vars
 //
-var $mainLoader = $('.wow-loader-main');
+var $mainLoader = $('.wow-loader.main');
 var $mainArea = $('.wow-container');
+var $mainError = $('.wow-error.main');
 
+var $heroSearch = $('#wow-search');
+var $realmSearch = $('#wow-search-realm');
+var $searchButton = $('#wow-search-submit');
+
+// Get Search
+//
+var searchable = 1;
+var characterSearched;
+var realmSearched;
+
+$heroSearch.on('keyup', function(e) {if (e.which === 13) {$searchButton.click()} });
+$searchButton.on('click', function() {
+  
+  if( $heroSearch.val() != '' && $realmSearch.val() != 'server' && searchable == 1 ) {
+    
+    $mainError.fadeOut(180);
+    $mainError.removeClass('active');
+    $mainLoader.fadeIn(180);
+    
+    characterSearched = $heroSearch.val().replace(/ /g,'').toLowerCase();
+    realmSearched = $realmSearch.val();
+    
+    console.log('Searching Character: ' + characterSearched);
+    console.log('On Realm: ' + realmSearched);
+    
+    setTimeout(function() {
+      searchable = 0;
+      var info = buildUrl('character', realmSearched, characterSearched);
+      getWowFromSearch(info);
+    },100);
+    
+  } else {
+    
+    if($mainError.hasClass('active')) {
+      $mainError.fadeOut(180);
+      setTimeout(function() {
+        $mainError.fadeIn(180);
+      },200);
+    } else {
+      $mainError.addClass('active');
+      $mainError.fadeIn(180);
+      $mainError.html('Error: Enter a Character Name and select a Realm');
+    }
+    
+    
+  }
+  
+});
+
+// Build URL
+//
+//https://us.api.battle.net/wow/character/chogall/milkme
+function buildUrl(calltype, server, name) {
+  
+  var baseUrl = 'https://us.api.battle.net/wow/';
+  var secretUrl = '?locale=en_US&jsonp=apiCalled&apikey=cp9c5gugfpezfeewpmj26bme5cehdvx4';
+  var finishedUrl = baseUrl + calltype + '/' + server + '/' + name + secretUrl;
+  
+  if(finishedUrl) {
+    console.log('url: ' + finishedUrl);
+    return finishedUrl;
+  }
+  
+}
 
 // WoW ajax call
 //
-function getWoW(character, realm) {
+function getWowFromSearch(info) {
+  
+  // Callback life
+  window.apiCalled = function(data) {
+    console.log('WoW api called');
+    console.log(data);
+  };
   
   // Do ajax call
-  // - requires: character(string), realm(string)
   $.ajax({
-    url: 'http://192.168.1.103:8124/',
-    dataType: "jsonp",
-    jsonpCallback: "_testcb",
+    url: info,
+    type: 'GET',
+    dataType: 'jsonp',
+    jsonp: 'callback',
+    jsonpCallback: 'apiCalled',
     cache: false,
     timeout: 5000,
 
     success: function(data) {
-
+      
+      $mainError.fadeOut(180);
+      $mainError.removeClass('active');
       $mainLoader.fadeOut(180);
-      $mainArea.append(data);
+      $mainArea.append(JSON.stringify(data));
+      
+      setTimeout(function() {
+        searchable = 1;
+      },150);
 
     },
 
-    error: function(jqXHR, textStatus, errorThrown) {
-
-      alert('error ' + textStatus + " " + errorThrown);
-
+    error: function(xhr, status, error) {
+      
+      $mainLoader.fadeOut(180);
+      $mainError.html('Error: ' + status + " " + error);
+      $mainError.fadeIn(180);
+      $mainError.addClass('active');
 
     }
   
